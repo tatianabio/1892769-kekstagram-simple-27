3959c832f16defda1660b7156075587132d24972
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-10-13 16:06:08 +0300

    Разбивает файл js на модули, добавляет точку входа

---
+++ js/data.js
@@ -0,0 +1,20 @@
+import { getRandomNumber } from './util.js';
+
+const PHOTOS_COUNT = 25;
+const LIKES_MIN_COUNT = 15;
+const LIKES_MAX_COUNT = 200;
+const COMMENTS_MIN_COUNT = 0;
+const COMMENTS_MAX_COUNT = 200;
+
+const createPhotoDescriptionItem = (_, index) => ({
+  description: `Описание фотографии: ${getRandomNumber(0, PHOTOS_COUNT)}`,
+  likes: getRandomNumber(LIKES_MIN_COUNT, LIKES_MAX_COUNT),
+  comments: getRandomNumber(COMMENTS_MIN_COUNT, COMMENTS_MAX_COUNT),
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+});
+
+const createPhotoDescriptionsArray = () =>
+  Array.from({ length: PHOTOS_COUNT }, createPhotoDescriptionItem);
+
+export { createPhotoDescriptionsArray };
