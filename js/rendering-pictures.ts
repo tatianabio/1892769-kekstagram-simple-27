280a02c52faa242b80c1938965bffd5d963a5e82
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-10-20 18:35:10 +0300

    Добавляет типы

--- js/rendering-pictures.ts
+++ js/rendering-pictures.ts
@@ -1,25 +1,28 @@
 import createPhotoDescriptionsArray, { IPhotoItem } from './data.js';
 
-const picturesContainer: HTMLDivElement | null =
-  document.querySelector('.pictures');
-const templatePicture = (
-  document.querySelector('#picture') as HTMLTemplateElement
-).content.querySelector('.picture') as HTMLLinkElement;
+const picturesContainer = document.querySelector<HTMLDivElement>('.pictures');
+const templatePicture = document
+  .querySelector<HTMLTemplateElement>('#picture')
+  ?.content.querySelector<HTMLLinkElement>('.picture');
 
 const picturesFragment: DocumentFragment = document.createDocumentFragment();
 
 const pictures = createPhotoDescriptionsArray();
 
 pictures.forEach(({ url, likes, comments }: IPhotoItem) => {
-  const newPicture = templatePicture.cloneNode(true) as HTMLLinkElement;
+  const newPicture = templatePicture?.cloneNode(true) as HTMLLinkElement;
 
-  (newPicture.querySelector('.picture__img') as HTMLImageElement).src = url;
-  (
-    newPicture.querySelector('.picture__likes') as HTMLSpanElement
-  ).textContent = `${likes}`;
-  (
-    newPicture.querySelector('.picture__comments') as HTMLSpanElement
-  ).textContent = `${comments}`;
+  const imgNode = newPicture.querySelector<HTMLImageElement>('.picture__img');
+  imgNode && (imgNode.src = url);
+
+  const likesNode =
+    newPicture.querySelector<HTMLSpanElement>('.picture__likes');
+  likesNode && (likesNode.textContent = `${likes}`);
+
+  const commentsNode =
+    newPicture.querySelector<HTMLSpanElement>('.picture__comments');
+  commentsNode && (commentsNode.textContent = `${comments}`);
+
   picturesFragment.append(newPicture);
 });
 
