2acbc807e47989cfd06de690f0696440b4c0d083
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-10-23 21:09:09 +0300

    Добавляет валидацию поля комментария

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -1,14 +1,15 @@
 import { isEscapeKey } from './util.js';
-
 const uploadInput = document.querySelector('#upload-file');
 const uploadModal = document.querySelector('.img-upload__overlay');
 const uploadCancelButton = document.querySelector('#upload-cancel');
+const commentTextArea = uploadModal.querySelector('.text__description');
+const uploadForm = document.querySelector('.img-upload__form');
 
 const onModalEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
+    evt.preventDefault();
     closeUploadForm();
   }
-  evt.preventDefault();
 };
 
 function openUploadForm() {
@@ -21,19 +22,32 @@ function closeUploadForm() {
   uploadModal.classList.add('hidden');
   document.body.classList.remove('modal-open');
   uploadInput.value = '';
+  commentTextArea.value = '';
   document.removeEventListener('keydown', onModalEscKeydown);
 }
 
 const onUploadInputChange = () => {
-  uploadInput.addEventListener('change', () => {
-    openUploadForm();
-  });
+  uploadInput.addEventListener('change', openUploadForm);
 };
 
 const onUploadCancelButtonClick = () => {
-  uploadCancelButton.addEventListener('click', () => {
-    closeUploadForm();
-  });
+  uploadCancelButton.addEventListener('click', closeUploadForm);
 };
+
+const pristine = new Pristine(uploadForm);
+
+const onUploadFormSubmit = (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    // eslint-disable-next-line no-console
+    console.log('Форма валидна!');
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('Форма не валидна!');
+  }
+};
+
+uploadForm.addEventListener('submit', onUploadFormSubmit);
 
 export { onUploadInputChange, onUploadCancelButtonClick };
